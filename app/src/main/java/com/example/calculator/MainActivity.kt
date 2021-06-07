7889fe922e02b675ac0b60f6774c4b9844f5b3d7
Jane-Nyambura <janekamaunyambura@gmail.com>
2021-06-07 09:11:29 +0300

    first commit

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -12,55 +12,75 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var answer=findViewById<TextView>(R.id.tvAnswer)
-        var num1=findViewById<EditText>(R.id.etNum1)
-        var num2=findViewById<EditText>(R.id.etNum2)
 
-        var btn1=findViewById<Button>(R.id.btnAdd)
-        var btn2=findViewById<Button>(R.id.btnMinus)
-        var btn3=findViewById<Button>(R.id.btnModulus)
-        var btn4=findViewById<Button>(R.id.btnMultiply)
+        val num1=findViewById<EditText>(R.id.etNum1)
+        val num2=findViewById<EditText>(R.id.etNum2)
 
+        val btn1=findViewById<Button>(R.id.btnAdd)
+        val btn2=findViewById<Button>(R.id.btnMinus)
+        val btn3=findViewById<Button>(R.id.btnModulus)
+        val btn4=findViewById<Button>(R.id.btnMultiply)
+        val answer=findViewById<TextView>(R.id.tvAnswer)
 
 
         btn1.setOnClickListener {
-            Toast.makeText(baseContext,"num", Toast.LENGTH_LONG).show()
+            if (num1.text.toString()==""){
+            Toast.makeText(baseContext,"enter a number", Toast.LENGTH_LONG).show()
+            }else if (num2.text.toString()==""){
+                Toast.makeText(baseContext,"enter a number", Toast.LENGTH_LONG).show()
+            }
+            else {
+                val number1 = num1.toString().toInt()
+                var number2 = num2.toString().toInt()
+                var result = number1 + number2
+                answer.text = ("$result")
+            }
 
-            var number1=num1.toString().toInt()
-            var number2=num2.toString().toInt()
-            var result=number1+number2
-            answer.text=("$result")
+            }
+            btn2.setOnClickListener {
+                if (num1.text.toString()==""){
+                    Toast.makeText(baseContext,"enter a number", Toast.LENGTH_LONG).show()
+                }else if (num2.text.toString()==""){
+                    Toast.makeText(baseContext,"enter a number", Toast.LENGTH_LONG).show()
+                }
+                else {
+                    val number1 = num1.toString().toInt()
+                    var number2 = num2.toString().toInt()
+                    var result = number1 - number2
+                    answer.text = ("$result")
+                }
 
+            }
 
-        }
-        btn2.setOnClickListener {
-            Toast.makeText(baseContext,"num", Toast.LENGTH_LONG).show()
-
-            var number1=num1.toString().toInt()
-            var number2=num2.toString().toInt()
-            var result=number1-number2
-            answer.text=("$result")
-
-
-        }
         btn3.setOnClickListener {
-            Toast.makeText(baseContext,"num", Toast.LENGTH_LONG).show()
-
-            var number1=num1.toString().toInt()
-            var number2=num2.toString().toInt()
-            var result=number1%number2
-            answer.text=("$result")
+            if (num1.text.toString()==""){
+                Toast.makeText(baseContext,"enter a number", Toast.LENGTH_LONG).show()
+            }else if (num2.text.toString()==""){
+                Toast.makeText(baseContext,"enter a number", Toast.LENGTH_LONG).show()
+            }
+            else {
+                val number1 = num1.toString().toInt()
+                var number2 = num2.toString().toInt()
+                var result = number1 % number2
+                answer.text = ("$result")
+            }
 
         }
         btn4.setOnClickListener {
-            Toast.makeText(baseContext,"num", Toast.LENGTH_LONG).show()
-
-            var number1=num1.toString().toInt()
-            var number2=num2.toString().toInt()
-            var result=number1/number2
-            answer.text=("$result")
-
+            if (num1.text.toString()==""){
+                Toast.makeText(baseContext,"enter a number", Toast.LENGTH_LONG).show()
+            }else if (num2.text.toString()==""){
+                Toast.makeText(baseContext,"enter a number", Toast.LENGTH_LONG).show()
+            }
+            else {
+                val number1 = num1.toString().toInt()
+                var number2 = num2.toString().toInt()
+                var result = number1 * number2
+                answer.text = ("$result")
+            }
 
         }
+        }
+
+
     }
-}
